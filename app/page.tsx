a2d522a6ee8cac1bf55d91071adf8b2c9e6e8b32
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2025-10-14 12:36:48 +0600

    Added text animation and cropped image

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,45 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mogra text-sm lg:flex">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <h1>HAPPY BIRTHDAY AZHAR!</h1>
-      </div>
-    </main>
+    <div id='balloon-container'>
+      <main className='flex min-h-screen flex-col items-center p-5 pt-30 gap-5'>
+        <h1 className='text-pink-500 dark:text-pink-300 font-mogra text-5xl text-center wave-text'>
+          <span>H</span>
+          <span>A</span>
+          <span>P</span>
+          <span>P</span>
+          <span>Y</span>
+          <span>&nbsp;</span>
+          <span>B</span>
+          <span>I</span>
+          <span>R</span>
+          <span>T</span>
+          <span>H</span>
+          <span>D</span>
+          <span>A</span>
+          <span>Y</span>
+          <span>&nbsp;</span>
+          <span>A</span>
+          <span>Z</span>
+          <span>H</span>
+          <span>A</span>
+          <span>R</span>
+          <span>!</span>
+          <span>!</span>
+          <span>!</span>
+        </h1>
+        <div className='flex items-center justify-between w-full max-w-xl'>
+          <Image
+            className='relative w-full h-auto'
+            src='/image.png'
+            alt='Azhar jumping'
+            width={200}
+            height={145}
+            priority
+          />
+        </div>
+      </main>
+    </div>
   );
 }
